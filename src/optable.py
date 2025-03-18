03b1f8ff2e358080059c7bca4e1b5d9a29212c33
ElectronixTM <etdiary1@gmail.com>
2025-03-18 03:50:11 +0300

    feat: добавил таблицу опкодов в первом варианте

---
+++ src/optable.py
@@ -0,0 +1,86 @@
+"""
+Файл, в котором захардкожены все опкоды, и соответствующие им операнды,
+а также добавлены несколько функций для удобства их извлечения. В целом
+наверняка между опкодами есть какие-то очевидные закономерности, которые
+позволяют не вбивать все возможности сюда, но к сожалению я их не увидел
+"""
+from dataclasses import dataclass
+from enum import IntEnum, auto
+from command import *
+from abstracts import Operation, Register, Condition
+from typing import TypeAlias
+
+# тип данных, который для каждого отдельного типа
+# опеации хранит различные варианты ее опкодов для
+# разных типов аргументов
+
+class OpcodeLayout(IntEnum):
+    MATH = auto()
+    MEMORY = auto()
+    BRANCHING = auto()
+
+@dataclass
+class OpcodeDescription:
+    opcode: int
+    oplayout: OpcodeLayout
+
+# несколько фиктивных типов для того, чтобы просто разделять разные опкоды
+# зависящие только от размера операнда, так как в питоне размер явно не
+# задается
+I8 = type("I8")
+I32 = type("I32")
+# LongMem - mem address with 32 bit dispp
+LMem = type("LMem")
+# ShortMem - mem address with 8 bit disp or without it
+SMem = type("LMem")
+
+OpTable: TypeAlias = dict[frozenset[type] | tuple[type, ...], OpcodeDescription]
+
+# Чтобы это дело не растягивалось на несколько километров, создам алиасы
+Lout = OpcodeLayout
+Desc = OpcodeDescription
+
+OP_TABLE: dict[str, OpTable] = {
+        "add": {
+            (Register, Register): Desc(0, Lout.MATH),
+            (Register, I8): Desc(10, Lout.MATH)
+            },
+        "sub": {
+            (Register, Register): Desc(1, Lout.MATH),
+            (Register, I8): Desc(11, Lout.MATH)
+            },
+        "mul": {
+            (Register, Register): Desc(2, Lout.MATH),
+            (Register, I8): Desc(12, Lout.MATH)
+            },
+        "div": {
+            (Register, Register): Desc(3, Lout.MATH),
+            (Register, I8): Desc(13, Lout.MATH)
+            },
+        "and": {
+            (Register, Register): Desc(4, Lout.MATH),
+            },
+        "or": {
+            (Register, Register): Desc(5, Lout.MATH),
+            },
+        "xor": {
+            (Register, Register): Desc(6, Lout.MATH),
+            },
+        "mask": {
+            (Register, Register): Desc(7, Lout.MATH),
+            },
+        "load": {
+            (Register, LMem): Desc(20, Lout.MEMORY),
+            (Register, SMem): Desc(30, Lout.MEMORY)
+        },
+        "store": {
+            (Register, LMem): Desc(21, Lout.MEMORY),
+            (Register, SMem): Desc(31, Lout.MEMORY)
+            },
+        "branch": {
+            (Condition, I32): Desc(40, Lout.MEMORY),
+            (Condition, I8): Desc(50, Lout.MEMORY),
+            (Condition, LMem): Desc(41, Lout.MEMORY)
+            }
+        }
+
