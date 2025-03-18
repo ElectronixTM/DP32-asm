2edb676b742c154785dfd638712ee4c18725d853
ElectronixTM <etdiary1@gmail.com>
2025-03-18 12:43:41 +0300

    fix: поправил сигнатуры типов для различных операций
    В основном это были забытые 3-и регистры назначения

--- src/optable.py
+++ src/optable.py
@@ -23,6 +23,7 @@ class OpcodeLayout(IntEnum):
 class OpcodeDescription:
     opcode: int
     oplayout: OpcodeLayout
+    expanded: bool = False
 
 # несколько фиктивных типов для того, чтобы просто разделять разные опкоды
 # зависящие только от размера операнда, так как в питоне размер явно не
@@ -47,46 +48,59 @@ Desc = OpcodeDescription
 
 OP_TABLE: dict[str, OpTable] = {
         "add": {
-            (Register, Register): Desc(0, Lout.MATH),
-            (Register, I8): Desc(10, Lout.MATH)
+            (Register, Register, Register): Desc(0, Lout.MATH),
+            (Register, Register, I8): Desc(10, Lout.MATH)
             },
         "sub": {
-            (Register, Register): Desc(1, Lout.MATH),
-            (Register, I8): Desc(11, Lout.MATH)
+            (Register, Register, Register): Desc(1, Lout.MATH),
+            (Register, Register, I8): Desc(11, Lout.MATH)
             },
         "mul": {
-            (Register, Register): Desc(2, Lout.MATH),
-            (Register, I8): Desc(12, Lout.MATH)
+            (Register, Register, Register): Desc(2, Lout.MATH),
+            (Register, Register, I8): Desc(12, Lout.MATH)
             },
         "div": {
-            (Register, Register): Desc(3, Lout.MATH),
-            (Register, I8): Desc(13, Lout.MATH)
+            (Register, Register, Register): Desc(3, Lout.MATH),
+            (Register, Register, I8): Desc(13, Lout.MATH)
             },
         "and": {
-            (Register, Register): Desc(4, Lout.MATH),
+            (Register, Register, Register): Desc(4, Lout.MATH),
             },
         "or": {
-            (Register, Register): Desc(5, Lout.MATH),
+            (Register, Register, Register): Desc(5, Lout.MATH),
             },
         "xor": {
-            (Register, Register): Desc(6, Lout.MATH),
+            (Register, Register, Register): Desc(6, Lout.MATH),
             },
         "mask": {
-            (Register, Register): Desc(7, Lout.MATH),
+            (Register, Register, Register): Desc(7, Lout.MATH),
             },
         "load": {
-            (Register, LMem): Desc(20, Lout.MEMORY),
+            (Register, LMem): Desc(20, Lout.MEMORY, expanded=True),
             (Register, SMem): Desc(30, Lout.MEMORY)
         },
         "store": {
-            (Register, LMem): Desc(21, Lout.MEMORY),
+            (Register, LMem): Desc(21, Lout.MEMORY, expanded=False),
             (Register, SMem): Desc(31, Lout.MEMORY)
             },
         "branch": {
             (Condition, I32): Desc(40, Lout.MEMORY),
             (Condition, I8): Desc(50, Lout.MEMORY),
-            (Condition, LMem): Desc(41, Lout.MEMORY)
-            #? (Condition, SMem): Desc(51, Lout.MEMORY) # В описании не указан, поэтому и у меня не будет
+            (Condition, LMem): Desc(41, Lout.MEMORY),
+            # conditions are effectively integers, so it's 
+            # also possible to just pass them directly
+            (I8, I32): Desc(40, Lout.MEMORY),
+            (I8, I8): Desc(50, Lout.MEMORY),
+            (I8, LMem): Desc(41, Lout.MEMORY)
+            #? (Condition, SMem): Desc(51, Lout.MEMORY) В описании
+            # не указан, поэтому и у меня не будет
             }
         }
 
+def get_opdesc(mnemonic: str, ops_types: tuple[type, ...]) -> OpcodeDescription:
+    """
+    Fetches appropriate operation description from `OP_TABLE`
+    using given info on operands and mnemonic
+    """
+    candidates = OP_TABLE[mnemonic]
+    return candidates[ops_types]
