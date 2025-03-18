0fa75b7a8e95189f639d0b58ae96f6e96146b1e7
ElectronixTM <etdiary1@gmail.com>
2025-03-18 12:42:39 +0300

    fix: добавил более корректную обработку размеров операций

--- src/codegenutils.py
+++ src/codegenutils.py
@@ -8,7 +8,7 @@ from abstracts import Register, Condition, Operation, Identifier, MemPtr
 import optable
 from command import Command, CommandSizes, MAX_FIELD_VAL
 
-def handle_math_opcode(
+def handle_math_op(
         opcode: int,
         r3: Register,
         r1: Register,
@@ -28,14 +28,15 @@ def handle_mem_op(
         opcode: int,
         r3: Register,
         r1: Register,
-        disp: int
+        disp: int,
+        size: CommandSizes = CommandSizes.DEFAULT
         ) -> Command:
     result = Command()
     result.opcode = opcode
     result.r3_or_flags = r3.index
     result.r1 = r1.index
-    if disp > MAX_FIELD_VAL:
-        result.size = CommandSizes.DOUBLED
+    result.size = size
+    if size == CommandSizes.DOUBLED:
         result.extra = disp
     else:
         result.r2_or_const = disp
@@ -45,24 +46,36 @@ def handle_branch_op(
         opcode: int,
         ivnz: Condition,
         r1: Register | None,
-        disp: int
+        disp: int,
+        size: CommandSizes = CommandSizes.DEFAULT
         ) -> Command:
     result = Command()
     result.opcode = opcode
     result.r3_or_flags = ivnz.cond
+    result.size = size
     if r1:
         result.r1 = r1.index
-    if disp > MAX_FIELD_VAL:
-        result.size = CommandSizes.DOUBLED
+    if size == CommandSizes.DOUBLED:
         result.extra = disp
     return result
 
-def _convert_candidates(
-        operand: Register | int | Condition | Identifier | MemPtr
+def typedef_candidate(
+        operand: Register | int | Condition | MemPtr | Identifier
         ) -> type:
+    """
+    Принимает все операнды класса Operation и преобразует их в типы
+    в соответствии с которыми они будут закодированы при ассемблировании
+    """
+    if isinstance(operand, Identifier):
+        return optable.I32
     if isinstance(operand, MemPtr):
-        # TODO: решить эту проблему
-        pass
+        if isinstance(operand.disp, Identifier):
+            return optable.LMem
+        elif operand.disp > MAX_FIELD_VAL:
+            return optable.LMem
+        else:
+            return optable.SMem
+
     if not isinstance(operand, int):
         return type(operand)
     if operand > MAX_FIELD_VAL:
@@ -70,21 +83,3 @@ def _convert_candidates(
     else:
         return optable.I8
 
-def assemble_operation(operation: Operation):
-    candidates = optable.OP_TABLE[operation.mnemonic]
-    operands = tuple(map(_convert_candidates, operation.operands))
-    print(operands)
-    winner = None
-    for candidate in candidates.keys():
-        if candidate == operands:
-            winner = candidates[candidate]
-            break
-    print(winner)
-
-
-if __name__ == "__main__":
-    o = Operation(
-            "add",
-            [Register(12), 7]
-            )
-    assemble_operation(o)
