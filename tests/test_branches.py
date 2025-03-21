3d617cbed3a99eda8de3830c9698aaa72bd3deec
ElectronixTM <etdiary1@gmail.com>
2025-03-21 03:58:31 +0300

    tests: начал писать тесты для ветвления

---
+++ tests/test_branches.py
@@ -0,0 +1,22 @@
+"""
+Проверка ассемблирования условных переходов. Сильно интереснее тут конечно
+прыжки по меткам, но если они работают отдельно, по идее должны работать и
+тут. Другой вопрос уже что это штука сложнее и может у меня branch не 
+работает вообще
+"""
+
+import pytest
+from src.abstracts import *
+from src.main import assemble
+
+@pytest.mark.parametrize("source,index,expected",
+                         [
+                             ("dw 0 0 x: dw 0 0 branch {i=1vnz} rel x", 5*4, "fffffffe"),
+                             ("dw 0 0 x: dw 0 0 branch {i=1vnz} -6", 4*4, "500f00fa")
+                         ]
+                         )
+def test_rel_addr(source, index, expected):
+    assembled = assemble(source)
+    addr = assembled[index:index+4]
+    print("HEX: ", assembled.hex())
+    assert addr.hex() == expected
