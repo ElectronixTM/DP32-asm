f5e33d09a4b4081360efb4a0603628e5a315ec4e
mrqiz <mrqiz@internet.ru>
2025-03-28 15:05:58 +0300

    fix: исправил относительную адресацию в ассемблере

--- src/assembler.py
+++ src/assembler.py
@@ -82,7 +82,7 @@ class Assembler:
     def _resolve_identifier(self, id_: Identifier) -> int:
         if not IdFlags.REL_ADDR in id_.flags:
             return self.GAT[id_]
-        return self.GAT[id_] - self._cur_addr
+        return self.GAT[id_] - self._cur_addr - 2
 
     def _resolve_mem_ptr(self, mem_ptr: MemPtr) -> MemPtr:
         """
